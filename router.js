7de099c550ee44459da14a166c8d673c5a1d1282
jessewar <jesse.warren@live.com>
2015-05-27 15:53:06 -0700

    switched from UDP to TCP

--- router.js
+++ router.js
@@ -1,22 +1,34 @@
-var dgram = require('dgram');
-var EventEmitter = require('events').EventEmitter;
-var emitter = new EventEmitter();
+var net = require('net');
 
 // Establish socket listening for HTTP connections
-var serverSocket = dgram.createSocket('udp4');
-serverSocket.bind(5555);
-serverSocket.on('listening', function() {
+var serverSocket = net.createServer();
+serverSocket.listen(5555, function() {
   var address = serverSocket.address();
   console.log('HTTP server socket listening on ' + address.address + ":" + address.port);
 });
 
 // Establish socket listening for Tor61 connections
-var torSocket = dgram.createSocket('udp4');
-torSocket.bind(1234);
-torSocket.on('listening', function() {
+var torSocket = net.createServer();
+torSocket.listen(1234, function() {
   var address = torSocket.address();
   console.log('Tor61 socket listening on ' + address.address + ":" + address.port);
 });
 
+// Returns a list of other routers available within the network
+function getAvailableRouters() {
+  return ['127.0.0.1 1234 1'];
+}
 
+var connectedRouters = [];
 
+// Construct circuit
+var availableRouters = getAvailableRouters();
+var routerInfo = availableRouters[0].split(' ');
+var routerAddress = routerInfo[0];
+var routerPort = parseInt(routerInfo[1]);
+var routerId = routerInfo[2];
+
+var circuitSocket;
+if (connectedRouters.indexOf(routerId) < 0) {
+  
+}
